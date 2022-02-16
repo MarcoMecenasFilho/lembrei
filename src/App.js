d765ccca2ed8971d58302216cf92512fcd81f51a
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-02-16 20:57:43 -0300

    fix bug node

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AppProvider from "./context/AppProvider";
 import TodoList from "./pages/TodoList";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
   return (
-    <BrowserRouter >
       <AppProvider>
         <Switch>
           <Route exact path="/" render={ () => <TodoList />} />
         </Switch>
       </AppProvider>
-    </BrowserRouter>
   );
 }
 
